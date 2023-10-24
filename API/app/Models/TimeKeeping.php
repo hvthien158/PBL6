6f297a7ae75a9f6cde1ba4e81c0abafcd087b4c1
Võ Đặng Hoàng Nguyên <kiaisoft.nguyen.vo@gmail.com>
2023-10-24 13:06:00 +0700

    create migration and model

---
+++ API/app/Models/TimeKeeping.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class TimeKeeping extends Model
+{
+    use HasFactory;
+    protected $table = 'time_keepings';
+    protected $fillable = [
+        'time_check_in',
+        'user_id'
+    ];
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function time_keepings(): HasMany
+    {
+        return $this->hasMany(TimeKeeping::class, 'shift_id');
+    }
+}
---
+++ API/database/migrations/2023_10_24_033105_create_time_keepings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_keepings', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('time_check_in');
+            $table->dateTime('time_check_out');
+            $table->text('note');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('shift_id')->references('id')->on('shifts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('time_keepings');
+    }
+};
