f7de5060a7a71c64f785a510a2e87474c7fa1e01
Trà Văn Thiên <kiaisoft.thien.tra@gmail.com>
2023-11-07 16:24:15 +0700

    Update ListUser.vue

--- Frontend/src/views/Admin/User/ListUser.vue
+++ Frontend/src/views/Admin/User/ListUser.vue
@@ -102,7 +102,7 @@ import router from "../../../router";
 import { useRoute } from "vue-router";
 import { useAlertStore } from "../../../stores/alert";
 import ConfirmBox from "../../../components/ConfirmBox.vue";
-const data = ref();
+const data = ref([]);
 const user = useUserStore().user;
 const alertStore = useAlertStore();
 const route = useRoute();
